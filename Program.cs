a0c6e21c053ceaff48a66ec44f488691fdb369c3
patmol <patmol@Debian.localdomain>
2020-02-13 12:40:15 -0800

    first commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace faceblur
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+	    foreach (var arg in args)  
+	    {
+                Console.WriteLine(arg);
+	    }
+        }
+    }
+}
